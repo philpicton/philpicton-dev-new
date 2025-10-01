62158e6ff01f7ebabe4d2c9576f0455fae0da52c
Phil Picton <hi@philpicton.dev>
2025-10-01 22:11:46 +0100

    add error page and start projects page

---
+++ error.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({ error: Object });
+</script>
+
+<template>
+  <NuxtLayout>
+    <main
+      class="prose dark:prose-invert prose-slate max-w-2xl px-3 py-7 mx-auto bg-white sm:px-8 sm:shadow dark:bg-slate-800 ring-1 ring-slate-200 dark:ring-slate-700 sm:rounded-lg prose-a:text-green-700 prose-a:hover:text-green-800 dark:prose-a:text-green-400 dark:prose-a:hover:text-green-500 flex flex-col items-center justify-center"
+    >
+      <h1 class="text-red-600 dark:text-red-400 mb-4">
+        Oh no! error {{ error.statusCode }}
+      </h1>
+      <p class="mb-6">
+        {{ error.statusMessage || "An unexpected error occurred." }}
+      </p>
+      <NuxtLink to="/" class="btn btn-green">Go Home</NuxtLink>
+    </main>
+  </NuxtLayout>
+</template>
+
+<style>
+@reference './assets/css/main.css';
+</style>
---
+++ pages/projects/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+showError({
+  statusCode: 302,
+  statusMessage: "This page is under construction.",
+});
+</script>
+
+<template>
+  <div>
+    <h1>This is the Projects page</h1>
+    <p>If you see this, the error handling did not work as expected.</p>
+  </div>
+</template>
